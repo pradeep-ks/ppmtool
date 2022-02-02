af5790141fd9602b8df8c996c83e5d86135ebea8
Pradeep Kumar <pradeepkumar0509@gmail.com>
2022-02-02 21:24:21 +0530

    Initial project setup

---
+++ ppmtool-api/src/main/java/io/javatech/ppmtool/api/PpmtoolApiApplication.java
@@ -0,0 +1,13 @@
+package io.javatech.ppmtool.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApiApplication.class, args);
+	}
+
+}
